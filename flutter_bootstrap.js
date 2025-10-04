d62f135bc63d2c3485fcb9c09867c06767d15668
pariwit12 <pariwit12@hotmail.com>
2025-10-04 17:07:13 +0700

    adjust style in Home and To-do List

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2768649160"
+    serviceWorkerVersion: "3903797254"
   }
 });
